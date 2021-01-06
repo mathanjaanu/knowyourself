3e03dc40bc65fd6cb063ffe39ce635382716f9d0
Mathan Kumar <mathan.kumar@landmarkwebteam.com>
2021-01-06 07:40:15 +0530

    changes2

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next Apps</title>
+        <title>Create Next Appss</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
